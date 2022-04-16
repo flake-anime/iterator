92813c7b22aa4ea36440ed423e2e55d9cfe57e6a
itsme12453 <74871622+itsme12453@users.noreply.github.com>
2022-04-16 09:50:29 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
 from engine.iterator import Iterator
+from database import Database
+import json
 
 iterator = Iterator()
-anime_data = iterator.get_a_to_z_list(1, 2)
+anime_data = iterator.get_a_to_z_list(1, 2, log=True)
 
-print(anime_data)
+database = Database("mongodb+srv://animeParadise:Paradise%40anime%3A%2F%2F@anime-paradise.ucpde.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+for counter, anime in enumerate(anime_data):
+    data = iterator.get_extra_info(anime["url"])
+    database.insert_data(data)
+    print(f"[*] {counter}/{len(anime_data)}")
